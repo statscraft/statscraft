d3e425959c35b712ec98adb0a2888d61a34c7b7e
erikzaadi <erik.zaadi@gmail.com>
2016-03-01 09:23:50 +0200

    Separate old event, add TBD for call for papers

--- travis.sh
+++ travis.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 rm -r ./public > /dev/null  || "no cleaning to do"
-$GOPATH/bin/hugo
+BASEURL="http://statscraft.org.il/"
+if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+    BASEURL="http://statscraft-temp.s3-website-us-east-1.amazonaws.com/${TRAVIS_BRANCH}/"
+fi
+$GOPATH/bin/hugo -b ${BASEURL}
