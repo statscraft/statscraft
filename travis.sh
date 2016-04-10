44a1e7ef229ee4932b3cf06a6ee16f3619c25797
erikzaadi <erik.zaadi@gmail.com>
2016-04-10 22:13:27 +0300

    Duck this cache thingy

--- travis.sh
+++ travis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -r ./public > /dev/null  || "no cleaning to do"
+rm -r ./public > /dev/null  || echo "no cleaning to do"
 BASEURL="http://www.statscraft.org.il/"
 if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
     BASEURL="http://statscraft-temp.s3-website-us-east-1.amazonaws.com/${TRAVIS_BRANCH}/"
