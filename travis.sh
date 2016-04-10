3f13b68e5283c633d695b0ae3b7b89b5bdf38b39
erikzaadi <erik.zaadi@gmail.com>
2016-04-10 22:03:35 +0300

    Fix the god damn build

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -r ./public > /dev/null  || "no cleaning to do"
-BASEURL="http://statscraft.org.il/"
+BASEURL="http://www.statscraft.org.il/"
 if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
     BASEURL="http://statscraft-temp.s3-website-us-east-1.amazonaws.com/${TRAVIS_BRANCH}/"
 fi
