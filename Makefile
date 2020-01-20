e5cec1684e982f758728b08506a1a8dfa6c4b1e7
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2020-01-20 17:10:40 +0200

    replace travis with github actions

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
 dev:
 	hugo serve --watch
 	# pushd public; python -m SimpleHTTPServer; popd
+prod:
+	hugo -b https://www.statscraft.org.il/
--- travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -r ./public > /dev/null  || echo "no cleaning to do"
-BASEURL="http://www.statscraft.org.il/"
-if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
-    BASEURL="http://statscraft-temp.s3-website-us-east-1.amazonaws.com/${TRAVIS_BRANCH}/"
-fi
-echo "Building with ${BASEURL} as baseurl"
-hugo -b ${BASEURL}
